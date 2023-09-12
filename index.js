8cba858b8e36bb22ca1eff3f06e8b303860f6682
Wondrech <143776837+Wondrech@users.noreply.github.com>
2023-09-13 02:59:38 +0800

    Update sound of game over

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function checkAnswer (currentLvl) {
             }, 2000);
         }
     } else {
-        playSound("wrong");
+        playSound("What A Shame");
 
         $("#level-title").text("Game over... Click here to restart.")
         
